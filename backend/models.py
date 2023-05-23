9bdb62b64af87c6cf573d3f92ecde194b91d47f3
imriadutta <riaduttacoder@gmail.com>
2023-05-24 01:13:58 +0530

    model change

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Video(models.Model):
     title = models.CharField(max_length=500)
-    content = models.FileField()
+    content = models.FileField(upload_to='media/')
     description = models.TextField()
     upload_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
